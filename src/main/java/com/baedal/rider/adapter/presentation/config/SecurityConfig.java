83b00404e01cb2330fa0ab7f29320baa12994248
handsone-u <handsoneu@gmail.com>
2025-04-14 17:22:48 +0900

    [#1] del: 필요없는 어노테이션 제거
    - @EnableWebSecurity: 없어도 등록됨.

--- src/main/java/com/baedal/rider/adapter/presentation/config/SecurityConfig.java
+++ src/main/java/com/baedal/rider/adapter/presentation/config/SecurityConfig.java
@@ -1,11 +1,9 @@
 package com.baedal.rider.adapter.presentation.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,8 +11,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
   @Bean
