de737d5dcb41d3ff9e13aa2ac8eaada8f1a4cd14
handsone-u <handsoneu@gmail.com>
2025-04-24 11:37:44 +0900

    [#6] add: redis 설정 추가

--- src/main/java/com/baedal/rider/config/SecurityConfig.java
+++ src/main/java/com/baedal/rider/config/SecurityConfig.java
@@ -1,9 +1,10 @@
-package com.baedal.rider.adapter.config;
+package com.baedal.rider.config;
 
 import com.baedal.rider.adapter.presentation.security.AuthFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
   @Value("${security.permit-all.urls}")
