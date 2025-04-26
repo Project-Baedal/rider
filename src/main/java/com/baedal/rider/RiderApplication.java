3c390cbde6c0a5462645980948d57d5395cac4f7
limjintaek <jintaek019@gmail.com>
2025-04-27 01:07:00 +0900

    [#8] add. OpenFeign 의존성 추가 및 적용

--- src/main/java/com/baedal/rider/RiderApplication.java
+++ src/main/java/com/baedal/rider/RiderApplication.java
@@ -2,10 +2,12 @@ package com.baedal.rider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableFeignClients
 public class RiderApplication {
 
 	public static void main(String[] args) {
